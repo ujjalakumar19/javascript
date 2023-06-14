b2f2f69b99bb6851dcabae1ed1b400e98eac1c91
ujjala kumari <134491878+ujjalakumar19@users.noreply.github.com>
2023-06-14 21:37:20 +0530

    Create file8.js

---
+++ file8.js
@@ -0,0 +1,25 @@
+// typeof operator 
+
+// data types (primitive data types)
+// string "harhit"
+// number 2, 4, 5.6 
+// booleans 
+// undefined
+// null 
+// BigInt
+// Symbol
+
+// let age = 22; 
+// let firstName = "harshit";
+
+// // console.log(typeof age);
+
+// // 22 -> "22"
+// // convert number to string. 
+// age = age + "";
+// console.log(typeof(age)); "22"
+
+
+// // convert string to number. 
+
+// let myStr = +"34";
